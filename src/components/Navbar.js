1db79550ab28b6a62430650ae598bbd0bd3cf108
“kiranmk3331” <kiranmk3331@gmail.com>
2023-04-24 10:58:33 +0530

    Add logout.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,45 @@
-import React from "react";
-import { Book, LogIn } from "react-feather";
+import React, { useContext } from "react";
+import { Book, LogIn, LogOut } from "react-feather";
+import { MyContext } from "../App";
 
 export const Navbar = () => {
+  const { isAuthenticated, setIsAuthenticated } = useContext(MyContext);
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  };
+
   return (
     <div className="navbar">
-      <div className="login">
-        <a href="/login">
-          <button>
-            <LogIn />
-            Login
-          </button>
-        </a>
-      </div>
-      <a href="/signup">
-        <div className="signup">
-          <button>
-            <Book />
-            Sign Up
+      {isAuthenticated ? (
+        <div className="logout">
+          <button onClick={handleLogout}>
+            <LogOut />
+            Logout
           </button>
         </div>
-      </a>
+      ) : (
+        <>
+          <a href="/login">
+            <div className="login">
+              <button>
+                <LogIn />
+                Login
+              </button>
+            </div>
+          </a>
+          <a href="/signup">
+            <div className="signup">
+              <button>
+                <Book />
+                Sign Up
+              </button>
+            </div>
+          </a>
+        </>
+      )}
     </div>
   );
 };
